249c816d3905c562e6e434ba9967173e17599301
h.t.jacky <h.t.jacky@gmail.com>
2014-01-25 22:19:36 +0800

    [Change] lab3-exercise 9

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -141,7 +141,9 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 
 		// Make sure this memory is valid.
 		// Return -1 if it is not.  Hint: Call user_mem_check.
-		// LAB 3: Your code here.
+		// LAB 3: Jacky 140125.
+		if(user_mem_check(curenv, usd, sizeof(struct UserStabData), PTE_U) < 0)
+			return -1;
 
 		stabs = usd->stabs;
 		stab_end = usd->stab_end;
@@ -150,6 +152,8 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 
 		// Make sure the STABS and string table memory is valid.
 		// LAB 3: Your code here.
+		if((user_mem_check(curenv, stabs, (uint32_t)stab_end - (uint32_t)stabs, PTE_U) < 0) || (user_mem_check(curenv, stabstr, (uint32_t)stabstr_end - (uint32_t)stabstr, PTE_U) < 0))
+			return -1;
 	}
 
 	// String table validity checks
--- kern/pmap.c
+++ kern/pmap.c
@@ -583,7 +583,21 @@ static uintptr_t user_mem_check_addr;
 int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
-	// LAB 3: Your code here.
+	// LAB 3: Jacky 140125.
+	uint32_t iter;
+	pte_t *pte;
+	if ((uint32_t)(va + len) >= ULIM) {
+		user_mem_check_addr = (uintptr_t)(va + len);
+		return -E_FAULT;
+	}
+	for (iter = (uint32_t)va; iter < (uint32_t)ROUNDUP((va + len), PGSIZE); iter += PGSIZE) {
+		pte = pgdir_walk(curenv->env_pgdir, (void *)iter, 0);
+		if ((pte == NULL) || ((*pte & perm) != perm)) {
+			cprintf("Need perm = %x, but *pte is %x!\n", perm, *pte);
+			user_mem_check_addr = iter;
+			return -E_FAULT;
+		}
+	}
 
 	return 0;
 }
